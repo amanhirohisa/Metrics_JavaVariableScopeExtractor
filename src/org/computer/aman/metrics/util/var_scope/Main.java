4c0bf81b7cf0c4bca23592e44aab672107504ff1
Hirohisa Aman <aman@ehime-u.ac.jp>
2021-07-26 18:37:56 +0900

    バージョン番号の修正漏れ

--- src/org/computer/aman/metrics/util/var_scope/Main.java
+++ src/org/computer/aman/metrics/util/var_scope/Main.java
@@ -16,7 +16,7 @@ import org.eclipse.jdt.core.dom.CompilationUnit;
 
 public class Main 
 {
-	public static final String VERSION = "3.2";
+	public static final String VERSION = "3.2.1";
 	public static final String COPYRIGHT = "(C) 2015-2021 Hirohisa AMAN <aman@computer.org>";
 
 	public static void main(String[] args) throws IOException 
